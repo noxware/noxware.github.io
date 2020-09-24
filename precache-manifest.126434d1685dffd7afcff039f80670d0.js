201dfc9d6a44d99466b753a220a639d33e7906ce
Noxware <fprofeti98@gmail.com>
2020-09-24 16:07:32 -0300

    Updates

---
+++ precache-manifest.126434d1685dffd7afcff039f80670d0.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef64624a937215d4c169a28b1686d2a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3e4cbcc70672aee044a",
+    "url": "/static/js/2.7a1dbb67.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7a1dbb67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79afebe2bf8b149565de",
+    "url": "/static/js/main.3fe4429b.chunk.js"
+  },
+  {
+    "revision": "bd852e7eb139d6cc279b",
+    "url": "/static/js/runtime-main.50ed05b9.js"
+  },
+  {
+    "revision": "6fa02da16641a348ae62c4f71628e0ad",
+    "url": "/static/media/Arduino.6fa02da1.svg"
+  },
+  {
+    "revision": "5f0e74a14f5d9dfbe06d500fa45176c2",
+    "url": "/static/media/Bash.5f0e74a1.svg"
+  },
+  {
+    "revision": "bdaccca278378c33d51a37613e6acd09",
+    "url": "/static/media/C#.bdaccca2.svg"
+  },
+  {
+    "revision": "c64a068ead3cf3210c7608cea6018583",
+    "url": "/static/media/C++.c64a068e.svg"
+  },
+  {
+    "revision": "00cc5bdb950c7cd82c90fbe74b26b3cf",
+    "url": "/static/media/C.00cc5bdb.svg"
+  },
+  {
+    "revision": "e788f0ec66d11079bb6a5782316d3e3b",
+    "url": "/static/media/CSS.e788f0ec.svg"
+  },
+  {
+    "revision": "dac6068bd08011a1d00d9f47688ebdae",
+    "url": "/static/media/Gimp.dac6068b.svg"
+  },
+  {
+    "revision": "64d5e98f1a220cdcad2b1a70d52ab669",
+    "url": "/static/media/Git.64d5e98f.svg"
+  },
+  {
+    "revision": "1f4630ca7f4e95a6cb2c95d3ac3e7947",
+    "url": "/static/media/HTML.1f4630ca.svg"
+  },
+  {
+    "revision": "a732a6e5bc69653a55d6dd81ab8d2d5e",
+    "url": "/static/media/Java.a732a6e5.svg"
+  },
+  {
+    "revision": "87711407412494d529d45ee76167583c",
+    "url": "/static/media/Javascript.87711407.svg"
+  },
+  {
+    "revision": "2e82d079f8bc66df87636c7bb8f8ab7c",
+    "url": "/static/media/Linux.2e82d079.svg"
+  },
+  {
+    "revision": "33743237f03981588dd7bedcc090e785",
+    "url": "/static/media/Lua.33743237.svg"
+  },
+  {
+    "revision": "dd2a5a358ea682bed45bf98bc842b3ed",
+    "url": "/static/media/MySQL.dd2a5a35.svg"
+  },
+  {
+    "revision": "94cafb0d1b4a2875215db17964f0938a",
+    "url": "/static/media/Node.js.94cafb0d.svg"
+  },
+  {
+    "revision": "d7c985e7cca077dc2d77f89a66573e35",
+    "url": "/static/media/PostgreSQL.d7c985e7.svg"
+  },
+  {
+    "revision": "8557d1d2472aa8ca94046d97bd0da4cf",
+    "url": "/static/media/Puppeteer.8557d1d2.svg"
+  },
+  {
+    "revision": "51c2eab22c8994e70e7633979982b451",
+    "url": "/static/media/Python.51c2eab2.svg"
+  },
+  {
+    "revision": "a1f299f2bbaff5e1f19e2f6f1bb0b5d0",
+    "url": "/static/media/Qt.a1f299f2.svg"
+  },
+  {
+    "revision": "7eb0d22d069ed9817d31e4a7aa202c5c",
+    "url": "/static/media/Raspberry Pi.7eb0d22d.svg"
+  },
+  {
+    "revision": "ee61ca49ec2188e4ac026017e8b315b7",
+    "url": "/static/media/React Native.ee61ca49.svg"
+  },
+  {
+    "revision": "ee61ca49ec2188e4ac026017e8b315b7",
+    "url": "/static/media/React.ee61ca49.svg"
+  },
+  {
+    "revision": "de37387d48dab2c1fb786bccb7c18499",
+    "url": "/static/media/Selenium.de37387d.svg"
+  },
+  {
+    "revision": "9e8bfce4de1e3508a98fb887c593c11b",
+    "url": "/static/media/Typescript.9e8bfce4.svg"
+  }
+]);
